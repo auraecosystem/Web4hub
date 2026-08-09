5bd153382ca6e56f4e12f2cd771b428fd2955275
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-09 17:51:05 +0100

    Initialize Pusher configuration in initializer

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '2184708'
+Pusher.key = '5551079ec0aee047975f'
+Pusher.secret = '60e1fbbd5d59f771e929'
+Pusher.cluster = 'mt1'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
